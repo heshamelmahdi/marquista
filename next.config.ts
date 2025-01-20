7b95ac620adff9a25447a5e28ff980e7f9bbaadc
Hesham El-Mahdi <heshamelmahdi@gmail.com>
2025-01-20 07:21:24 -0500

    final updates to next config

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,5 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
   images: {
@@ -25,6 +26,13 @@ const nextConfig: NextConfig = {
         hostname: "storage.googleapis.com",
       },
     ],
+  },
+  webpack: (config) => {
+    config.resolve.alias["@"] = path.resolve(__dirname, "src");
+    return config;
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
   },
 };
 
